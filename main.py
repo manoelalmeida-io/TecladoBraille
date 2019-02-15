85e724805a0a75dbbd08b2043af3ac5ead519de7
Rafael Reis <rafaelreis278@gmail.com>
2019-02-14 22:16:19 -0200

    backup

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import setup
+import random
+import words as word
 from pynput import keyboard
 import simpleaudio as sa
+
+palavra = ''
+mode = 0
+words = word.word
 
 
 def main():
@@ -9,6 +15,7 @@ def main():
         setup.generate_files()
 
     print('Teclado iniciado')
+    play_async('sound/phrases/Bem_vindo.wav')
 
     with keyboard.Listener(
             on_press=on_press) as listener:
@@ -17,17 +24,83 @@ def main():
 
 def on_press(key):
 
-    try:
-        play_async('sound/keys/{}.wav'.format(str.upper(key.char)))
+    global palavra
+    global mode
 
-    except AttributeError as error:
-        pass
+    if mode == 0:
+        learn(key)
+    else:
+        game(key)
 
 
 def play_async(file):
     sa.stop_all()
     wave_obj = sa.WaveObject.from_wave_file(file)
     wave_obj.play()
+
+
+def game(key):
+
+    global palavra
+    global mode
+
+    try:
+        global words
+
+        if key == keyboard.Key.space:
+            mode = 0
+            print('APRENDER')
+
+        if key == keyboard.Key.enter:
+            if palavra == words[0]:
+                print('correto!')
+                shuffle()
+            else:
+                if palavra == '':
+                    shuffle()
+                else:
+                    print('errado')
+                    print(words[0])
+            palavra = ''
+
+        if key == keyboard.Key.backspace:
+            palavra = palavra[:-1]
+
+        play_async('sound/keys/{}.wav'.format(str.upper(key.char)))
+        palavra += key.char
+
+
+
+    except AttributeError as error:
+        pass
+    except FileNotFoundError as e:
+        pass
+
+
+def shuffle():
+
+    global words
+
+    random.shuffle(words)
+    print(words[0])
+
+
+def learn(key):
+
+    global mode
+
+    try:
+        if key == keyboard.Key.space:
+            mode = 1
+            print('JOGAR')
+            shuffle()
+
+        play_async('sound/keys/{}.wav'.format(str.upper(key.char)))
+
+    except AttributeError as error:
+        pass
+    except FileNotFoundError as e:
+        pass
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 import os
+import words as word
 import subprocess
 from gtts import gTTS
 
 
 def check_files():
+
+    words = word.word
 
     chars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
              'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -11,6 +14,13 @@ def check_files():
     for c in chars:
 
         wav_path = 'sound/keys/{}.wav'.format(c)
+
+        if not os.path.exists(wav_path):
+            return False
+
+    for c in words:
+
+        wav_path = 'sound/words/{}.wav'.format(c)
 
         if not os.path.exists(wav_path):
             return False
@@ -32,6 +42,11 @@ def generate_files():
     if not os.path.exists('sound/phrases'):
         os.makedirs('sound/phrases')
 
+    if not os.path.exists('sound/words'):
+        os.makedirs('sound/words')
+
+    words = word.word
+
     chars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
              'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
@@ -39,10 +54,25 @@ def generate_files():
 
     for index, c in enumerate(chars):
 
-        print('Gerando arquivos. {}/26'.format(index), end='\r')
+        print('Gerando arquivos de letras. {}/26'.format(index), end='\r')
 
         mp3_path = 'sound/keys/{}.mp3'.format(c)
         wav_path = 'sound/keys/{}.wav'.format(c)
+
+        tts = gTTS(text=c, lang='pt-br')
+        tts.save(mp3_path)
+
+        subprocess.call(['ffmpeg', '-y', '-i', mp3_path, wav_path], stdout=devnull, stderr=devnull)
+
+        if os.path.exists(mp3_path):
+            os.remove(mp3_path)
+
+    for index, c in enumerate(words):
+
+        print('Gerando arquivos de palavras. {}/{}'.format(index, len(words)), end='\r')
+
+        mp3_path = 'sound/words/{}.mp3'.format(c)
+        wav_path = 'sound/words/{}.wav'.format(c)
 
         tts = gTTS(text=c, lang='pt-br')
         tts.save(mp3_path)
@@ -59,6 +89,7 @@ def generate_files():
     tts.save(mp3_intro)
 
     subprocess.call(['ffmpeg', '-y', '-i', mp3_intro, wav_intro], stdout=devnull, stderr=devnull)
+
 
     if os.path.exists(mp3_intro):
         os.remove(mp3_intro)
